4dc36b969acbf1e842c830c3b4b95bd64aa2c9b0
jzuriaga <juazuri@gmail.com>
2016-12-18 11:37:43 +0100

    Remove trailing chars

--- src/reducers/instance_reducer.js
+++ src/reducers/instance_reducer.js
@@ -5,7 +5,7 @@ function instanceByApp (app) {
   switch (app) {
     case 'app1':
       return ["00", "01"];
-    case 'app2': case 'app3': 
+    case 'app2': case 'app3':
       return ["00","01","02","03","04","05"];
   }
   return [];
@@ -14,11 +14,11 @@ function instanceByApp (app) {
 
 export default function (state=[], action) {
   switch (action.type) {
-  case CLEAR_GRAPHS: 
+  case CLEAR_GRAPHS:
     return [];
   case SELECT_APP:
     return instanceByApp(action.payload);
-  case SELECT_POOL: 
+  case SELECT_POOL:
     switch (action.payload.pool) {
       case 'pre': case 'test':
         return [];
