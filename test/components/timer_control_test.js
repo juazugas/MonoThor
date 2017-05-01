d17957b820dfa7a1a1cf97e9f4639617f8c2b707
juazugas <juazuri@gmail.com>
2017-05-01 12:54:57 +0200

    Complete Test for TimerControl.

--- test/components/timer_control_test.js
+++ test/components/timer_control_test.js
@@ -1,15 +1,19 @@
+import sinon from 'sinon';
 import {renderComponent, expect} from '../test_helper';
+
+import { START_TIMER, STOP_TIMER } from '../../src/actions/types';
 import TimerControl from '../../src/components/timer_control';
 
 describe('TimerControl', () => {
 
   let component;
   let props;
+
   beforeEach(() => {
     props = {
-      timer: '',
-      startTimer: () => {},
-      stopTimer: () => {}
+      timer: START_TIMER,
+      startTimer: sinon.spy(),
+      stopTimer: sinon.spy()
     };
     component = renderComponent(TimerControl, props);
   });
@@ -18,14 +22,52 @@ describe('TimerControl', () => {
     expect(component).to.have.class('timer-control');
   });
 
-  it('should have a pause button', () => {
-    expect(component.find('button[title=Pause]')).to.exist;
+  it('should have a pause button active', () => {
+    let button = component.find('button[title=Pause]');
+    expect(button).to.exist;
+    expect(button).to.not.be.diabled;
   });
 
   it('should have a resume button disabled', () => {
     let button = component.find('button[title=Resume]');
     expect(button).to.exist;
     expect(button).to.be.diabled;
+  });
+
+  describe('pausing', () => {
+
+    let pauseButton;
+    beforeEach(() => {
+      component.find('button[title=Pause]').simulate('click');
+      props.timer = STOP_TIMER;
+      component = renderComponent(TimerControl, props);
+      pauseButton = component.find('button[title=Pause]');
+    });
+
+    it('should call stopTimer function', () => {
+      expect(props.stopTimer.calledOnce).to.be.truthy;
+    });
+
+    it('should disable pause button when props.timer == STOP_TIMER', () => {
+      expect(pauseButton).to.be.disabled;
+    });
+
+    it('should enable resume button when props.timer == STOP_TIMER', () => {
+      expect(component.find('button[title=Resume]')).to.not.be.disabled;
+    });
+
+  });
+
+  describe('resuming', () => {
+
+    beforeEach(() => {
+      component.find('button[title=Resume]').simulate('click');
+    });
+
+    it('should call startTimer function', () => {
+      expect(props.startTimer.calledOnce).to.be.truthy;
+    });
+
   });
 
 });
